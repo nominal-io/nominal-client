31e403fd876baf24bb1e5a9740d2b3660f1da7e5
Alexander Reynolds <alex@theory.shop>
2024-09-16 17:48:51 -0700

    accidentally uploaded the csv twice

--- tests/e2e/test_sdk.py
+++ tests/e2e/test_sdk.py
@@ -73,7 +73,6 @@ def test_add_csv_to_dataset(csv_data, csv_data2):
     ds.poll_until_ingestion_completed(interval=timedelta(seconds=0.1))
 
     with mock.patch("builtins.open", mock.mock_open(read_data=csv_data2)):
-        ds = nm.upload_csv("fake_path.csv", name, "timestamp", "iso_8601", desc)
         ds.add_csv_to_dataset("fake_path.csv", "timestamp", "iso_8601")
     ds.poll_until_ingestion_completed(interval=timedelta(seconds=0.1))
 
