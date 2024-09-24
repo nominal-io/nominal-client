2c8df1ec883a8a55ab00af519c934e23da0a9c96
Alexander Reynolds <alex@theory.shop>
2024-09-24 14:22:23 -0400

    documentation once-over on ts.py

--- nominal/ts.py
+++ nominal/ts.py
@@ -30,6 +30,7 @@ __all__ = [
 ]
 
 IntegralNanosecondsUTC: TypeAlias = int
+"""A timestamp in nanoseconds since the Unix epoch, UTC."""
 
 
 class _ConjureTimestampDomain(abc.ABC):
@@ -40,12 +41,20 @@ class _ConjureTimestampDomain(abc.ABC):
 
 @dataclass(frozen=True)
 class Iso8601(_ConjureTimestampDomain):
+    """ISO 8601 timestamp format, e.g. '2021-01-01T00:00:00Z' or '2021-01-01T00:00:00.123+00:00'.
+    The time zone must be specified.
+    """
+
     def _to_conjure_ingest_api(self) -> ingest_api.TimestampType:
         return ingest_api.TimestampType(absolute=ingest_api.AbsoluteTimestamp(iso8601=ingest_api.Iso8601Timestamp()))
 
 
 @dataclass(frozen=True)
 class Epoch(_ConjureTimestampDomain):
+    """An absolute timestamp in numeric format representing time since some epoch.
+    The timestamp can be integral or floating point, e.g. 1612137600.123 for 2021-02-01T00:00:00.123Z.
+    """
+
     unit: _LiteralTimeUnit
 
     def _to_conjure_ingest_api(self) -> ingest_api.TimestampType:
@@ -55,13 +64,17 @@ class Epoch(_ConjureTimestampDomain):
 
 @dataclass(frozen=True)
 class Relative(_ConjureTimestampDomain):
+    """A relative timestamp in numeric format representing time since some start time.
+    The relative timestamp can be integral or floating point, e.g. 12.123 for 12 seconds and 123 milliseconds after start.
+    The start time is absolute timestamp format representing time since some epoch.
+    """
+
     unit: _LiteralTimeUnit
     start: datetime | IntegralNanosecondsUTC
     """The starting time to which all relatives times are relative to."""
 
     def _to_conjure_ingest_api(self) -> ingest_api.TimestampType:
-        """
-        Note: The offset is a conjure datetime. They are serialized as ISO-8601 strings, with up-to nanosecond precision.
+        """Note: The offset is a conjure datetime. They are serialized as ISO-8601 strings, with up-to nanosecond precision.
         The Python type for the field is just a str.
         Ref:
         - https://github.com/palantir/conjure/blob/master/docs/concepts.md#built-in-types
@@ -75,14 +88,22 @@ class Relative(_ConjureTimestampDomain):
 
 @dataclass(frozen=True)
 class Custom(_ConjureTimestampDomain):
+    """A custom timestamp format. The custom timestamps are expected to be absolute timestamps.
+
+    The format string should be in the format of the `DateTimeFormatter` class in Java.
+    """
+
     format: str
+    """Must be in the format of the `DateTimeFormatter` class in Java."""
     default_year: int | None = None
+    """Accepted as an optional field for cases like IRIG time format, where the year is not present."""
 
     def _to_conjure_ingest_api(self) -> ingest_api.TimestampType:
         fmt = ingest_api.CustomTimestamp(format=self.format, default_year=self.default_year)
         return ingest_api.TimestampType(absolute=ingest_api.AbsoluteTimestamp(custom_format=fmt))
 
 
+# constants for pedagogy, documentation, default arguments, etc.
 ISO_8601 = Iso8601()
 EPOCH_NANOSECONDS = Epoch("nanoseconds")
 EPOCH_MICROSECONDS = Epoch("microseconds")
@@ -120,7 +141,10 @@ _LiteralRelativeDeprecated: TypeAlias = Literal[
 ]
 
 TypedTimeDomain: TypeAlias = Union[Iso8601, Epoch, Relative, Custom]
+"""Strongly typed time domain types."""
+
 _AnyTimeDomain: TypeAlias = Union[TypedTimeDomain, _LiteralAbsolute, _LiteralRelativeDeprecated]
+"""All allowable time domain types, including string representations."""
 
 
 def _make_typed_time_domain(domain: _AnyTimeDomain) -> TypedTimeDomain:
@@ -164,6 +188,11 @@ _str_to_type: Mapping[_LiteralAbsolute | _LiteralRelativeDeprecated, Iso8601 | E
 
 
 class _SecondsNanos(NamedTuple):
+    """A simple internal timestamp representation that can be converted to/from various formats.
+
+    These represent nanosecond-precision epoch timestamps.
+    """
+
     seconds: int
     nanos: int
 
@@ -174,7 +203,11 @@ class _SecondsNanos(NamedTuple):
         return ingest_api.UtcTimestamp(seconds_since_epoch=self.seconds, offset_nanoseconds=self.nanos)
 
     def to_iso8601(self) -> str:
-        """datetime.datetime objects are only microsecond-precise, so we use numpy's datetime64[ns] for nanosecond precision."""
+        """datetime.datetime is only microsecond-precise, so we use np.datetime64[ns] to get nanosecond-precision for printing.
+        Note that nanosecond precision is the maximum allowable for conjure datetime fields.
+        - https://github.com/palantir/conjure/blob/master/docs/concepts.md#built-in-types
+        - https://github.com/palantir/conjure/pull/1643
+        """
         return str(np.datetime64(self.to_integral_nanoseconds(), "ns")) + "Z"
 
     def to_integral_nanoseconds(self) -> IntegralNanosecondsUTC:
