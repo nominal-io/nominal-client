5b73cb3e9d40435b3e2b3aef6b11af73cee2da74
Alexander Reynolds <alex@theory.shop>
2024-09-27 16:33:31 -0400

    add e2e test for substring check

--- tests/e2e/test_toplevel.py
+++ tests/e2e/test_toplevel.py
@@ -181,6 +181,19 @@ def test_search_runs():
     assert run2.labels == run.labels == ()
 
 
+def test_search_runs_substring():
+    name = f"run-{uuid4()}"
+    desc = f"top-level test to search for a run by name {uuid4()}"
+    start, end = _create_random_start_end()
+    run = nm.create_run(name, start, end, desc)
+    runs = nm.search_runs(name_substring=name[4:])
+    assert len(runs) == 1
+    run2 = runs[0]
+
+    assert run2.rid == run.rid != ""
+    assert run2.name == run.name == name
+
+
 def test_upload_attachment(csv_data):
     at_title = f"attachment-{uuid4()}"
     at_desc = f"top-level test to upload an attachment {uuid4()}"
