568d5e0412bc40260ff7df5c0a39b833f94c493a
Alexander Reynolds <alex@theory.shop>
2024-10-04 17:06:51 -0400

    missed some future imports

--- nominal/core/_utils.py
+++ nominal/core/_utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Iterable, Protocol, TypeVar, runtime_checkable
 
 from .._utils import logger
--- nominal/core/user.py
+++ nominal/core/user.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 from .._api.combined import authentication_api
