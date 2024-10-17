28ce423ea60d46b9f3e3c964719b15547e920948
Drake Eidukas <drake@nominal.io>
2024-10-16 19:28:12 -0700

    Add utilities to get the validity and expiration date of the auth token

--- nominal/core/_clientsbunch.py
+++ nominal/core/_clientsbunch.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import datetime
+import logging
 from dataclasses import dataclass
 from functools import partial
 
-from conjure_python_client import RequestsClient, ServiceConfiguration
+import jwt
+from conjure_python_client import ConjureHTTPError, RequestsClient, ServiceConfiguration
 from typing_extensions import Self
 
 from .._api.combined import (
@@ -22,6 +25,8 @@ from .._api.combined import (
     timeseries_logicalseries_api,
     upload_api,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -64,3 +69,39 @@ class ClientsBunch:
             upload=client_factory(upload_api.UploadService),
             video=client_factory(scout_video.VideoService),
         )
+
+    def auth_expiration(self) -> datetime.datetime:
+        """Returns the timestamp at which the current auth token becomes invalid
+
+        Raises:
+            ValueError: Malformed auth token present
+        """
+        token_parts = self.auth_header.split()
+        if len(token_parts) != 2:
+            raise ValueError(f"Could not parse auth header: expected two space-separated parts")
+
+        token = token_parts[1]
+        payload = jwt.decode(token, options={"verify_signature": False}, algorithms=["RS256"])
+        expiry_s = payload.get("exp")
+        if expiry_s is None:
+            raise ValueError("Expected expiry to be present in auth token, but not present")
+
+        return datetime.datetime.fromtimestamp(expiry_s, datetime.UTC)
+
+    def auth_valid(self) -> bool:
+        """Validates that authentication tokens are valid and we can successfully hit the nominal API."""
+        try:
+            # simplest possible API call with minimal overhead to validate conectivity and
+            # proper authentication
+            self.authentication.get_my_profile(self.auth_header)
+            return True
+        except ConjureHTTPError as ex:
+            status_code = ex.response.status_code
+            if status_code == 401:
+                logger.error(
+                    "Invalid authentication token: please visit 'https://app.gov.nominal.io/settings/user?tab=tokens' and set a new one with nom auth set-token"
+                )
+            elif status_code != 200:
+                logger.error(f"Unknown error {status_code} received when validating authentication token")
+
+        return False
