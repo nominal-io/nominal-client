353e5f7fa95e1ba0df820c3def904d147d7c40d8
Alexander Reynolds <alex@theory.shop>
2024-10-22 11:31:34 -0400

    feat: add Channel and Connection to top level (#100)
    add Channel and Connection to top level
    hide from toplevel docs

--- nominal/core/__init__.py
+++ nominal/core/__init__.py
@@ -1,6 +1,8 @@
 from nominal.core.attachment import Attachment
+from nominal.core.channel import Channel
 from nominal.core.checklist import Check, Checklist, ChecklistBuilder
 from nominal.core.client import NominalClient
+from nominal.core.connection import Connection
 from nominal.core.dataset import Dataset, poll_until_ingestion_completed
 from nominal.core.log import Log, LogSet
 from nominal.core.run import Run
@@ -9,9 +11,11 @@ from nominal.core.video import Video
 
 __all__ = [
     "Attachment",
+    "Channel",
     "Check",
     "Checklist",
     "ChecklistBuilder",
+    "Connection",
     "Dataset",
     "Log",
     "LogSet",
