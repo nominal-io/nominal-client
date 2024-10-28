506c8f2d2dde183aacb0386afe446e209ed6abb4
Ben Epstein <ben.epstein97@gmail.com>
2024-10-27 20:51:13 -0400

    async push to sink

--- stream.py
+++ stream.py
@@ -4,6 +4,7 @@ import sys
 import time
 from typing import Callable
 from threading import Thread, Event
+from concurrent.futures import ThreadPoolExecutor
 from uuid import uuid4
 import json
 
@@ -30,23 +31,33 @@ class NominalWriteStream:
         self.source = source
         self.sink = f"sink_{uuid4()}.jsonl"
         self.stop_event = Event()
+        self.pool = ThreadPoolExecutor(max_workers=10)
         self.thread = Thread(target=self.worker, args=(self.stop_event,))
         self.thread.start()
 
+    def _write_sink(self, write_req_data: dict) -> None:
+        """Threaded entrypoint to write to the sync in the pool."""
+        time.sleep(0.5) # simulate some network request lag
+        with open(self.sink, "a") as sink:
+            json.dump(write_req_data, sink)
+            sink.write('\n')
 
     def worker(self, stop_event: Event) -> None:
         """Worker process."""
         while not stop_event.is_set():
             source_data = self.source()
             write_req_data = {"batches": [source_data], "dataSourceRid": self.data_source_id}
-            with open(self.sink, "a") as sink:
-                json.dump(write_req_data, sink)
-                sink.write('\n')
+            self.pool.submit(self._write_sink, write_req_data)
             time.sleep(self.push_freq_sec)
 
-    def stop(self) -> None:
-        """Gracefully kill the thread."""
+    def stop(self, cancel_running: bool = False) -> None:
+        """Gracefully kill the thread.
+        
+        Args:
+            cancel_running (bool): If True, cancel the running threads writing to the data source.
+        """
         self.stop_event.set()
+        self.pool.shutdown(cancel_futures=cancel_running)
         self.thread.join()
 
 
@@ -65,3 +76,16 @@ def write_stream(source: Callable[[], dict], data_source_id: str, push_freq_sec:
     stream = NominalWriteStream(source, data_source_id, push_freq_sec)
     print(f"Pushing to sink {stream.sink}")
     return stream
+
+
+if __name__ == "__main__":
+    # Read at a higher frequency than the sink can handle. We'll ensure that no messages are dropped
+    stream = write_stream(read_source, "data-id1", push_freq_sec=0.1)
+    time.sleep(2)
+    stream.stop()
+    with open(stream.sink, "r") as f:
+        data = [json.loads(line) for line in f.readlines()]
+    seconds = [row["batches"][0]["points"]["double"][0]["timestamp"]["seconds"] for row in data]
+    assert seconds == list(range(len(data)))
+    print("Success!")
+    
