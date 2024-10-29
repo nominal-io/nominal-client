02dd9dcdb0bccefad56f2a85ddb16e789d095833
Ben Epstein <ben.epstein97@gmail.com>
2024-10-28 21:19:57 -0400

    more intense test

--- stream.py
+++ stream.py
@@ -1,23 +1,22 @@
 import random
-import requests
-import sys
 import time
 from typing import Callable
 from threading import Thread, Event
 from concurrent.futures import ThreadPoolExecutor
 from uuid import uuid4
 import json
+import random
 
 
 ## User Code
-seconds = 0
+SECONDS = 0
 points_per_batch = 10
 
 def read_source() -> dict:
-    global seconds
-    points = [{'timestamp': {'seconds': seconds, 'nanos': i}, 'value': random.random()} for i in range(points_per_batch)]
+    global SECONDS
+    points = [{'timestamp': {'seconds': SECONDS, 'nanos': i}, 'value': random.random()} for i in range(points_per_batch)]
     batch = {'channel': 'streaming-test-channel-0', 'tags': {}, 'points': {'type': 'double', 'double': points}}
-    seconds += 1
+    SECONDS += 1
     return batch
 ## End user code
 
@@ -37,7 +36,8 @@ class NominalWriteStream:
 
     def _write_sink(self, write_req_data: dict) -> None:
         """Threaded entrypoint to write to the sync in the pool."""
-        time.sleep(0.5) # simulate some network request lag
+        sleep_time = random.randint(0, 4) + 0.3 # some major fluctuation in request latency
+        time.sleep(sleep_time) # simulate some network request lag
         with open(self.sink, "a") as sink:
             json.dump(write_req_data, sink)
             sink.write('\n')
@@ -80,12 +80,15 @@ def write_stream(source: Callable[[], dict], data_source_id: str, push_freq_sec:
 
 if __name__ == "__main__":
     # Read at a higher frequency than the sink can handle. We'll ensure that no messages are dropped
-    stream = write_stream(read_source, "data-id1", push_freq_sec=0.1)
-    time.sleep(2)
+    stream = write_stream(read_source, "data-id1", push_freq_sec=0.01)
+    time.sleep(3)
+    print("Stopping...")
     stream.stop()
+    print("Done.")
     with open(stream.sink, "r") as f:
         data = [json.loads(line) for line in f.readlines()]
-    seconds = [row["batches"][0]["points"]["double"][0]["timestamp"]["seconds"] for row in data]
-    assert seconds == list(range(len(data)))
+    batch_seconds = [row["batches"][0]["points"]["double"][0]["timestamp"]["seconds"] for row in data]
+    assert sorted(batch_seconds) == list(range(len(data)))
+    assert SECONDS == len(batch_seconds) # ensure we didn't drop any records from the sink
     print("Success!")
     
