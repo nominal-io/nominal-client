fbe25c6b62762747e4d47b566ec42fdf5a008a1b
Drake Eidukas <drake@nominal.io>
2024-10-29 15:22:11 -0700

    Add todo to move logic for building nominal_url into factory function

--- nominal/core/dataset.py
+++ nominal/core/dataset.py
@@ -64,6 +64,7 @@ class Dataset(HasRid):
     @property
     def nominal_url(self) -> str:
         """Returns a URL to the page in the nominal app containing this dataset"""
+        # TODO (drake): move logic into _from_conjure() factory function to accomodate different URL schemes
         return f"https://app.gov.nominal.io/data-sources/{self.rid}"
 
     def poll_until_ingestion_completed(self, interval: timedelta = timedelta(seconds=1)) -> None:
