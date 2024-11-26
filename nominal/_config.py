0cac531299b62018e46c17269db42ce8a4ce9a3d
Ben Epstein <ben.epstein97@gmail.com>
2024-11-25 22:40:26 -0500

    feat: remove pydantic for dataclasses (#153)

--- nominal/_config.py
+++ nominal/_config.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from dataclasses import asdict, dataclass
 from pathlib import Path
 
-import pydantic
 import yaml
 from typing_extensions import Self  # typing.Self in 3.11+
 
@@ -11,7 +11,8 @@ from nominal.exceptions import NominalConfigError
 _DEFAULT_NOMINAL_CONFIG_PATH = Path("~/.nominal.yml").expanduser().resolve()
 
 
-class NominalConfig(pydantic.BaseModel):
+@dataclass
+class NominalConfig:
     environments: dict[str, str]
     """environments map base_urls (with no scheme) to auth tokens"""
 
@@ -21,13 +22,13 @@ class NominalConfig(pydantic.BaseModel):
             return cls(environments={})
         with open(path) as f:
             obj = yaml.safe_load(f)
-        return cls.model_validate(obj)
+        return cls(**obj)
 
     def to_yaml(self, path: Path = _DEFAULT_NOMINAL_CONFIG_PATH, create: bool = True) -> None:
         if create:
             path.touch()
         with open(path, "w") as f:
-            yaml.dump(self.model_dump(), f)
+            yaml.dump(asdict(self), f)
 
     def set_token(self, url: str, token: str, save: bool = True) -> None:
         if url.startswith("http"):
