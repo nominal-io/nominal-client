1987724f683a44d37ac5979dff6135d045313cbd
Max Najork <max@nominal.io>
2025-01-02 09:19:44 -0800

    feat: bump conjure definitions (#182)
    chore: bump conjure definitions

--- nominal/_api/scout_service_api/event/__init__.py
+++ nominal/_api/scout_service_api/event/__init__.py
@@ -1,10 +1,18 @@
 # coding=utf-8
 from .._impl import (
+    event_ArchiveEvent as ArchiveEvent,
     event_CreateEvent as CreateEvent,
     event_Event as Event,
     event_EventRid as EventRid,
     event_EventService as EventService,
     event_EventType as EventType,
     event_GetEvents as GetEvents,
+    event_SearchEventsRequest as SearchEventsRequest,
+    event_SearchEventsResponse as SearchEventsResponse,
+    event_SearchQuery as SearchQuery,
+    event_SearchQueryVisitor as SearchQueryVisitor,
+    event_SortField as SortField,
+    event_SortOptions as SortOptions,
+    event_UpdateEvent as UpdateEvent,
 )
 
