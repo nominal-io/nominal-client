2540eeb0a150e0eb5caf59f7c4959b7db6040f80
Varun Tupuri <varun@nominal.io>
2025-02-24 15:05:17 -0500

    PEMDAS

--- nominal/experimental/stream_v2/_write_stream.py
+++ nominal/experimental/stream_v2/_write_stream.py
@@ -150,16 +150,16 @@ class WriteStreamV2:
         """
         timestamp_normalized = _SecondsNanos.from_flexible(timestamp).to_nanoseconds()
         current_time_ns = time.time_ns()
-        enqueue_dict_timestamp_diff = current_time_ns - timestamp_normalized / 1e9
+        enqueue_dict_timestamp_diff = current_time_ns - timestamp_normalized
 
         for channel, value in channel_values.items():
             self.enqueue(channel, timestamp, value)
 
         current_time_ns = time.time_ns()
-        last_enqueue_timestamp_diff = current_time_ns - timestamp_normalized / 1e9
+        last_enqueue_timestamp_diff = current_time_ns - timestamp_normalized
 
-        self._add_metric_impl("enque_dict_start_staleness", timestamp_normalized, enqueue_dict_timestamp_diff)
-        self._add_metric_impl("enque_dict_end_staleness", timestamp_normalized, last_enqueue_timestamp_diff)
+        self._add_metric_impl("enque_dict_start_staleness", timestamp_normalized, enqueue_dict_timestamp_diff / 1e9)
+        self._add_metric_impl("enque_dict_end_staleness", timestamp_normalized, last_enqueue_timestamp_diff / 1e9)
 
     def __enter__(self) -> WriteStreamV2:
         """Create the stream as a context manager."""
