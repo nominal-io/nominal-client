3e0337d5932200d2e8dc08f3dfd008741b5d52a1
Joseph Giordano <21144246+joseph-gio@users.noreply.github.com>
2025-02-25 09:59:17 -0800

    ruff fixes

--- nominal/nominal.py
+++ nominal/nominal.py
@@ -584,7 +584,7 @@ def search_events(
     labels: Sequence[str] | None = None,
     properties: Mapping[str, str] | None = None,
 ) -> list[Event]:
-    "Searches for events that match the given filters"
+    """Searches for events that match the given filters"""
     conn = get_default_client()
     return conn.search_events(
         search_text=search_text, after=after, before=before, asset=asset, labels=labels, properties=properties
