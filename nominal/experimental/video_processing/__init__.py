acedb7eceed62870ea6075d0b4e8df26f7255ff4
Drake Eidukas <drake@nominal.io>
2025-03-10 13:28:04 -0700

    feat: add functionality to experimental package for normalizing video (#256)
    * feat: add functionality to experimental package for normalizing video
    
    * Fix typing
    
    * Drop opencv
    
    * Use shlex
    
    * Formatting
    
    * Future annotations

---
+++ nominal/experimental/video_processing/__init__.py
@@ -0,0 +1,7 @@
+from nominal.experimental.video_processing.video_conversion import frame_count, has_audio_track, normalize_video
+
+__all__ = [
+    "frame_count",
+    "has_audio_track",
+    "normalize_video",
+]
