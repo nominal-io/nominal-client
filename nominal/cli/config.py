bd87a5b732726f020dba9564ee957a48b3704b62
Alexander Reynolds <alex@theory.shop>
2025-03-24 21:58:44 -0400

    remove migrate command

--- nominal/cli/config.py
+++ nominal/cli/config.py
@@ -4,7 +4,7 @@ import dataclasses
 
 import click
 
-from nominal import _config as _deprecated_config
+
 from nominal import config
 from nominal.cli.util.global_decorators import global_options
 from nominal.cli.util.verify_connection import validate_token_url
@@ -47,23 +47,3 @@ def remove_profile(profile: str) -> None:
     new_cfg = dataclasses.replace(cfg, profiles={k: v for k, v in cfg.profiles.items() if k != profile})
     new_cfg.to_yaml()
     click.secho(f"Removed profile {profile} from {config.DEFAULT_NOMINAL_CONFIG_PATH}", fg="green")
-
-
-@config_cmd.command()
-def migrate() -> None:
-    """Interactively migrate deprecated config at ~/.nominal.yml
-    to new profile-based config at ~/.config/nominal/config.yml
-    """
-    deprecated_cfg = _deprecated_config.NominalConfig.from_yaml()
-    profiles = {}
-    for url, token in deprecated_cfg.environments.items():
-        if click.prompt(f"Add profile for {url}?", default="y", type=bool):
-            name = click.prompt("Profile name")
-            new_url = click.prompt("Base url", default=f"https://{url}")
-            new_token = click.prompt("Token", default=token)
-            if click.prompt("Validate connection?", default="y", type=bool):
-                validate_token_url(new_token, new_url)
-            profiles[name] = config.ConfigProfile(new_url, new_token)
-    new_cfg = config.NominalConfig(profiles=profiles, version=2)
-    new_cfg.to_yaml()
-    click.secho(f"Migrated config to {config.DEFAULT_NOMINAL_CONFIG_PATH}", fg="green")
