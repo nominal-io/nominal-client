38a5da950a028cf93f0154f58291ea25fa6d7e6d
Alexander Reynolds <alex@theory.shop>
2025-03-25 00:50:19 -0400

    move pandas channel function

--- nominal/io/core/channel.py
+++ nominal/io/core/channel.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
+from datetime import datetime
 import enum
 from dataclasses import dataclass, field
-from datetime import datetime
-from typing import Any, BinaryIO, Protocol, cast
+from typing import BinaryIO, Protocol, cast
 
-import pandas as pd
 from nominal_api import (
     api,
     datasource_api,
@@ -15,12 +14,11 @@ from nominal_api import (
     timeseries_channelmetadata_api,
     timeseries_logicalseries_api,
 )
-from nominal_api.api import Timestamp
 from typing_extensions import Self
 
 from nominal.io.core._clientsbunch import HasAuthHeader
 from nominal.io.core._utils import update_dataclass
-from nominal.io.ts import _MAX_TIMESTAMP, _MIN_TIMESTAMP, IntegralNanosecondsUTC, _SecondsNanos
+from nominal.io.ts import _SecondsNanos, IntegralNanosecondsUTC
 
 
 class ChannelDataType(enum.Enum):
@@ -84,30 +82,6 @@ class Channel:
         update_dataclass(self, updated_channel, fields=self.__dataclass_fields__)
         return self
 
-    def to_pandas(
-        self,
-        start: datetime | IntegralNanosecondsUTC | None = None,
-        end: datetime | IntegralNanosecondsUTC | None = None,
-    ) -> pd.Series[Any]:
-        """Retrieve the channel data as a pandas.Series.
-
-        The index of the series is the timestamp of the data.
-        The index name is "timestamp" and the series name is the channel name.
-
-        Example:
-        -------
-        ```
-        s = channel.to_pandas()
-        print(s.name, "mean:", s.mean())
-        ```
-
-        """
-        start_time = _MIN_TIMESTAMP.to_api() if start is None else _SecondsNanos.from_flexible(start).to_api()
-        end_time = _MAX_TIMESTAMP.to_api() if end is None else _SecondsNanos.from_flexible(end).to_api()
-        body = self._get_series_values_csv(start_time, end_time)
-        df = pd.read_csv(body, parse_dates=["timestamp"], index_col="timestamp")
-        return df[self.name]
-
     @classmethod
     def _from_conjure_datasource_api(cls, clients: _Clients, channel: datasource_api.ChannelMetadata) -> Self:
         # NOTE: intentionally ignoring archetype RID as it does not correspond to a Channel in the same way that a
@@ -150,47 +124,6 @@ class Channel:
             data_type=channel_data_type,
             _clients=clients,
         )
-
-    def get_decimated(
-        self,
-        start: str | datetime | IntegralNanosecondsUTC,
-        end: str | datetime | IntegralNanosecondsUTC,
-        *,
-        buckets: int | None = None,
-        resolution: int | None = None,
-    ) -> pd.DataFrame:
-        """Retrieve the channel data as a pandas.DataFrame, decimated to the given buckets or resolution.
-
-        Enter either the number of buckets or the resolution for the output.
-        Resolution in picoseconds for picosecond-granularity dataset, nanoseconds otherwise.
-        """
-        if buckets is not None and resolution is not None:
-            raise ValueError("Either buckets or resolution should be provided")
-
-        result = self._decimate_request(start, end, buckets, resolution)
-
-        # when there are less than 1000 points, the result is numeric
-        if result.numeric is not None:
-            df = pd.DataFrame(
-                result.numeric.values,
-                columns=["value"],
-                index=[_to_pandas_timestamp(timestamp) for timestamp in result.numeric.timestamps],
-            )
-            df.index.name = "timestamp"
-            return df
-
-        if result.bucketed_numeric is None:
-            raise ValueError("Unexpected response from compute service, bucketed_numeric should not be None")
-        df = pd.DataFrame(
-            [
-                (bucket.min, bucket.max, bucket.mean, bucket.count, bucket.variance)
-                for bucket in result.bucketed_numeric.buckets
-            ],
-            columns=["min", "max", "mean", "count", "variance"],
-            index=[_to_pandas_timestamp(timestamp) for timestamp in result.bucketed_numeric.timestamps],
-        )
-        df.index.name = "timestamp"
-        return df
 
     def _decimate_request(
         self,
@@ -282,56 +215,6 @@ class Channel:
         # note: the response is the same as the requests.Response.raw field, with stream=True on the request;
         # this acts like a file-like object in binary-mode.
         return cast(BinaryIO, response)
-
-
-def _get_series_values_csv(
-    auth_header: str,
-    client: scout_dataexport_api.DataExportService,
-    rid_to_name: dict[str, str],
-    start: api.Timestamp,
-    end: api.Timestamp,
-) -> BinaryIO:
-    request = scout_dataexport_api.ExportDataRequest(
-        channels=scout_dataexport_api.ExportChannels(
-            time_domain=scout_dataexport_api.ExportTimeDomainChannels(
-                channels=[
-                    scout_dataexport_api.TimeDomainChannel(
-                        column_name=name,
-                        compute_node=scout_compute_api.Series(raw=scout_compute_api.Reference(name=name)),
-                    )
-                    for name in rid_to_name.values()
-                ],
-                merge_timestamp_strategy=scout_dataexport_api.MergeTimestampStrategy(
-                    # only one series will be returned, so no need to merge
-                    none=scout_dataexport_api.NoneStrategy(),
-                ),
-                output_timestamp_format=scout_dataexport_api.TimestampFormat(
-                    iso8601=scout_dataexport_api.Iso8601TimestampFormat()
-                ),
-            )
-        ),
-        start_time=start,
-        end_time=end,
-        context=scout_compute_api.Context(
-            function_variables={},
-            variables={
-                name: scout_compute_api.VariableValue(series=scout_compute_api.SeriesSpec(rid=rid))
-                for rid, name in rid_to_name.items()
-            },
-        ),
-        format=scout_dataexport_api.ExportFormat(csv=scout_dataexport_api.Csv()),
-        resolution=scout_dataexport_api.ResolutionOption(
-            undecimated=scout_dataexport_api.UndecimatedResolution(),
-        ),
-    )
-    response = client.export_channel_data(auth_header, request)
-    # note: the response is the same as the requests.Response.raw field, with stream=True on the request;
-    # this acts like a file-like object in binary-mode.
-    return cast(BinaryIO, response)
-
-
-def _to_pandas_timestamp(timestamp: Timestamp) -> pd.Timestamp:
-    return pd.Timestamp(timestamp.seconds, unit="s", tz="UTC") + pd.Timedelta(timestamp.nanos, unit="ns")
 
 
 def _create_series_from_channel(
--- nominal/thirdparty/pandas/_pandas.py
+++ nominal/thirdparty/pandas/_pandas.py
@@ -1,14 +1,17 @@
 from __future__ import annotations
 
+from datetime import datetime
 from threading import Thread
-from typing import BinaryIO
+from typing import Any, BinaryIO
 
 import pandas as pd
 from nominal.io import ts
 from nominal.io._utils import reader_writer
+from nominal.io.core.channel import Channel
 from nominal.io.core.client import NominalClient
 from nominal.io.core.dataset import Dataset
 from nominal.io.core.filetype import FileTypes
+from nominal_api.api import Timestamp
 
 
 def upload_pandas(
@@ -51,3 +54,75 @@ def upload_pandas(
     if wait_until_complete:
         dataset.poll_until_ingestion_completed()
     return dataset
+
+
+def channel_to_series(
+    channel: Channel,
+    start: datetime | ts.IntegralNanosecondsUTC | None = None,
+    end: datetime | ts.IntegralNanosecondsUTC | None = None,
+) -> pd.Series[Any]:
+    """Retrieve the channel data as a pandas.Series.
+
+    The index of the series is the timestamp of the data.
+    The index name is "timestamp" and the series name is the channel name.
+
+    Example:
+    -------
+    ```
+    s = channel_to_series(channel)
+    print(s.name, "mean:", s.mean())
+    ```
+
+    """
+    start_time = ts._MIN_TIMESTAMP.to_api() if start is None else ts._SecondsNanos.from_flexible(start).to_api()
+    end_time = ts._MAX_TIMESTAMP.to_api() if end is None else ts._SecondsNanos.from_flexible(end).to_api()
+    body = channel._get_series_values_csv(start_time, end_time)
+    df = pd.read_csv(body, parse_dates=["timestamp"], index_col="timestamp")
+    return df[channel.name]
+
+
+def channel_to_series_decimated(
+    channel: Channel,
+    start: str | datetime | ts.IntegralNanosecondsUTC,
+    end: str | datetime | ts.IntegralNanosecondsUTC,
+    *,
+    buckets: int | None = None,
+    resolution: int | None = None,
+) -> pd.DataFrame:
+    """Retrieve the channel data as a pandas.DataFrame, decimated to the given buckets or resolution.
+
+    Enter either the number of buckets or the resolution for the output.
+    Resolution in picoseconds for picosecond-granularity dataset, nanoseconds otherwise.
+    """
+    if buckets is not None and resolution is not None:
+        raise ValueError("Either buckets or resolution should be provided")
+
+    result = channel._decimate_request(start, end, buckets, resolution)
+
+    # when there are less than 1000 points, the result is numeric
+    # TODO(alkasm): why should this return differently shaped dataframes?
+    if result.numeric is not None:
+        df = pd.DataFrame(
+            result.numeric.values,
+            columns=["value"],
+            index=[_to_pandas_timestamp(timestamp) for timestamp in result.numeric.timestamps],
+        )
+        df.index.name = "timestamp"
+        return df
+
+    if result.bucketed_numeric is None:
+        raise ValueError("Unexpected response from compute service, bucketed_numeric should not be None")
+    df = pd.DataFrame(
+        [
+            (bucket.min, bucket.max, bucket.mean, bucket.count, bucket.variance)
+            for bucket in result.bucketed_numeric.buckets
+        ],
+        columns=["min", "max", "mean", "count", "variance"],
+        index=[_to_pandas_timestamp(timestamp) for timestamp in result.bucketed_numeric.timestamps],
+    )
+    df.index.name = "timestamp"
+    return df
+
+
+def _to_pandas_timestamp(timestamp: Timestamp) -> pd.Timestamp:
+    return pd.Timestamp(timestamp.seconds, unit="s", tz="UTC") + pd.Timedelta(timestamp.nanos, unit="ns")
