77c6308bcbee1951097840dab754456904932445
Drake Eidukas <drake@nominal.io>
2025-03-28 15:01:28 -0700

    Formatting

--- tests/core/test_dataset.py
+++ tests/core/test_dataset.py
@@ -1,11 +1,8 @@
-import functools
 from datetime import timedelta
 from unittest.mock import MagicMock, patch
 
 import pytest
-from nominal_api.timeseries_channelmetadata_api import BatchUpdateChannelMetadataRequest
 
-from nominal.core.channel import Channel
 from nominal.core.dataset import Dataset, DatasetBounds
 from nominal.core.unit import Unit
 from nominal.exceptions import NominalIngestError, NominalIngestFailed
