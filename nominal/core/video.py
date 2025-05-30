13a31bf521a910a3de0ac85a96bd28d8b0734699
Stefan van der Walt <stefanv@berkeley.edu>
2025-05-29 23:26:14 -0700

    Allow MCAP files to be ingested as video.

--- nominal/core/video.py
+++ nominal/core/video.py
@@ -141,7 +141,7 @@ class Video(HasRid):
         file_type = FileType.from_video(path)
 
         with path.open("rb") as video_file:
-            return self.add_to_video_from_io(
+            return self.add_from_io(
                 video_file,
                 name=path_upload_name(path, file_type),
                 start=start,
@@ -232,7 +232,7 @@ class Video(HasRid):
         file_type = FileType.from_video(path)
 
         with path.open("rb") as video_file:
-            return self.add_mcap_to_video_from_io(
+            return self.add_mcap_from_io(
                 video_file,
                 name=path_upload_name(path, file_type),
                 topic=topic,
