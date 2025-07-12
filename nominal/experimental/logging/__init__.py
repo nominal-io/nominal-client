aee908d274964c1774fceb3e6234a97bb046f844
Drake Eidukas <drake@nominal.io>
2025-07-12 10:03:42 -0500

    feat: add nominal log handler for pythonically uploading logs to nominal (#410)
    * feat: add nominal log handler for pythonically uploading logs to nominal
    
    * fix formatting, move click log handler into nominal.experimental.logging
    
    Also addresses misc. pr comments
    
    * Improved threading semantics
    
    * flush remaining batches upon program exit
    
    * last pr comments

---
+++ nominal/experimental/logging/__init__.py
@@ -0,0 +1,4 @@
+from nominal.experimental.logging.click_log_handler import ClickLogHandler, install_click_log_handler
+from nominal.experimental.logging.nominal_log_handler import NominalLogHandler, install_nominal_log_handler
+
+__all__ = ["ClickLogHandler", "install_click_log_handler", "NominalLogHandler", "install_nominal_log_handler"]
