61f8e1770a1a1723feb649ba6e6f05b93fc62afc
Drake Eidukas <drake@nominal.io>
2025-08-22 10:42:09 -0500

    delete tests for now deprecated code that is failing

--- tests/core/test_dataset.py
+++ tests/core/test_dataset.py
@@ -41,69 +41,6 @@ def mock_dataset(mock_clients):
     return ds
 
 
-@patch("time.sleep", return_value=None)
-def test_poll_until_ingestion_completed_success(mock_sleep: MagicMock, mock_dataset: Dataset):
-    mock_dataset._clients.catalog.get_ingest_progress_v2.return_value = MagicMock(
-        ingest_status=MagicMock(type="success")
-    )
-
-    mock_dataset.poll_until_ingestion_completed(interval=timedelta(seconds=1))
-    mock_dataset._clients.catalog.get_ingest_progress_v2.assert_called()
-
-
-@patch("time.sleep", return_value=None)
-def test_poll_until_ingestion_completed_in_progress(mock_sleep: MagicMock, mock_dataset: Dataset):
-    mock_dataset._clients.catalog.get_ingest_progress_v2.side_effect = [
-        MagicMock(ingest_status=MagicMock(type="inProgress")),
-        MagicMock(ingest_status=MagicMock(type="inProgress")),
-        MagicMock(ingest_status=MagicMock(type="success")),
-    ]
-
-    mock_dataset.poll_until_ingestion_completed(interval=timedelta(seconds=1))
-
-    assert mock_dataset._clients.catalog.get_ingest_progress_v2.call_count == 3
-    assert mock_sleep.call_count == 2
-
-
-@patch("time.sleep", return_value=None)
-def test_poll_until_ingestion_completed_error(mock_sleep: MagicMock, mock_dataset: Dataset):
-    mock_dataset._clients.catalog.get_ingest_progress_v2.side_effect = [
-        MagicMock(
-            ingest_status=MagicMock(type="error", error=MagicMock(message="Ingest failed", error_type="type_error"))
-        ),
-        MagicMock(ingest_status=MagicMock(type="inProgress")),
-    ]
-
-    with pytest.raises(NominalIngestFailed) as e:
-        mock_dataset.poll_until_ingestion_completed(interval=timedelta(seconds=1))
-    assert str(e.value) == "ingest failed for dataset 'test-rid': Ingest failed (type_error)"
-    mock_sleep.assert_not_called()
-
-
-@patch("time.sleep", return_value=None)
-def test_poll_until_ingestion_completed_error_is_none(mock_sleep: MagicMock, mock_dataset: Dataset):
-    mock_dataset._clients.catalog.get_ingest_progress_v2.side_effect = [
-        MagicMock(ingest_status=MagicMock(type="error", error=None)),
-        MagicMock(ingest_status=MagicMock(type="inProgress")),
-    ]
-
-    with pytest.raises(NominalIngestError) as e:
-        mock_dataset.poll_until_ingestion_completed(interval=timedelta(seconds=1))
-    assert str(e.value) == "ingest status type marked as 'error' but with no instance for dataset 'test-rid'"
-    mock_sleep.assert_not_called()
-
-
-@patch("time.sleep", return_value=None)
-def test_poll_until_ingestion_completed_unknown_status(mock_sleep: MagicMock, mock_dataset: Dataset):
-    mock_dataset._clients.catalog.get_ingest_progress_v2.return_value = MagicMock(
-        ingest_status=MagicMock(type="unknown_status")
-    )
-
-    with pytest.raises(NominalIngestError, match="unhandled ingest status 'unknown_status' for dataset 'test-rid'"):
-        mock_dataset.poll_until_ingestion_completed(interval=timedelta(seconds=1))
-    mock_sleep.assert_not_called()
-
-
 def test_write_logs_more_than_batch(mock_dataset: Dataset):
     endpoint = Mock()
     mock_dataset._clients.storage_writer.write_logs = endpoint
