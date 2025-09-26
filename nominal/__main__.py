efd32765ce494fd7991350ee28f4f904a74b17bf
Alexander Reynolds <alex@theory.shop>
2025-09-26 01:20:48 -0400

    -m nominal -> -m nominal.cli

--- nominal/__main__.py
+++ nominal/__main__.py
@@ -1,4 +1,6 @@
 from nominal.cli import nom
+import warnings
 
 if __name__ == "__main__":
+    warnings.warn("`python -m nominal` is deprecated and will be removed in a future version. Use `python -m nominal.cli` instead.")
     nom()
