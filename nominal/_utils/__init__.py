1ba97f010bbc17cf756b8730a8ccc95fe872628d
Drake Eidukas <drake@nominal.io>
2025-10-29 09:09:25 -0500

    feat: implement multi-part downloader for dataset files (#509)
    * feat: implement multi-part downloader for dataset files
    
    * use multiprocessing cpu count instead of os
    
    * pr feedback
    
    * pr feedbakc round 2
    
    * Make threads write their own parts
    
    * pr feedback round 3
    
    * improve error handling

--- nominal/_utils/__init__.py
+++ nominal/_utils/__init__.py
@@ -1,6 +1,5 @@
 from nominal._utils.dataclass_tools import update_dataclass
 from nominal._utils.deprecation_tools import deprecate_arguments, warn_on_deprecated_argument
-from nominal._utils.download_tools import download_presigned_uri, filename_from_uri
 from nominal._utils.iterator_tools import batched
 from nominal._utils.streaming_tools import reader_writer
 from nominal._utils.timing_tools import LogTiming
@@ -8,8 +7,6 @@ from nominal._utils.timing_tools import LogTiming
 __all__ = [
     "batched",
     "deprecate_arguments",
-    "download_presigned_uri",
-    "filename_from_uri",
     "LogTiming",
     "reader_writer",
     "update_dataclass",
