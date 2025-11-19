620d0e3bd7a60849a5c722090e19b675b92587ea
Drake Eidukas <drake@nominal.io>
2025-11-19 11:24:29 -0600

    chore: reorder methods in the asset and run classes

--- nominal/core/run.py
+++ nominal/core/run.py
@@ -60,10 +60,48 @@ class Run(HasRid, RefreshableMixin[scout_run_api.Run]):
     def _get_latest_api(self) -> scout_run_api.Run:
         return self._clients.run.get_run(self._clients.auth_header, self.rid)
 
+    def update(
+        self,
+        *,
+        name: str | None = None,
+        start: datetime | IntegralNanosecondsUTC | None = None,
+        end: datetime | IntegralNanosecondsUTC | None = None,
+        description: str | None = None,
+        properties: Mapping[str, str] | None = None,
+        labels: Sequence[str] | None = None,
+        links: Sequence[str | Link | LinkDict] | None = None,
+    ) -> Self:
+        """Replace run metadata.
+        Updates the current instance, and returns it.
+        Only the metadata passed in will be replaced, the rest will remain untouched.
+
+        Links can be URLs, tuples of (URL, name), or dicts of {url=URL, title=name}.
+
+        Note: This replaces the metadata rather than appending it. To append to labels or properties, merge them before
+        calling this method. E.g.:
+
+            new_labels = ["new-label-a", "new-label-b"]
+            for old_label in run.labels:
+                new_labels.append(old_label)
+            run = run.update(labels=new_labels)
+        """
+        request = scout_run_api.UpdateRunRequest(
+            description=description,
+            labels=None if labels is None else list(labels),
+            properties=None if properties is None else dict(properties),
+            start_time=None if start is None else _SecondsNanos.from_flexible(start).to_scout_run_api(),
+            end_time=None if end is None else _SecondsNanos.from_flexible(end).to_scout_run_api(),
+            title=name,
+            assets=[],
+            links=None if links is None else create_links(links),
+        )
+        updated_run = self._clients.run.update_run(self._clients.auth_header, request, self.rid)
+        return self._refresh_from_api(updated_run)
+
     def _list_datasource_rids(
         self, datasource_type: str | None = None, property_name: str | None = None
     ) -> Mapping[str, str]:
-        enriched_run = self._clients.run.get_run(self._clients.auth_header, self.rid)
+        enriched_run = self._get_latest_api()
         datasource_rids_by_ref_name = {}
         for ref_name, source in enriched_run.data_sources.items():
             if datasource_type is not None and source.data_source.type != datasource_type:
@@ -75,6 +113,42 @@ class Run(HasRid, RefreshableMixin[scout_run_api.Run]):
             datasource_rids_by_ref_name[ref_name] = rid
 
         return datasource_rids_by_ref_name
+
+    def remove_data_sources(
+        self,
+        *,
+        ref_names: Sequence[str] | None = None,
+        data_sources: Sequence[Connection | Dataset | Video | str] | None = None,
+    ) -> None:
+        """Remove data sources from this run.
+
+        The list data_sources can contain Connection, Dataset, Video instances, or rids as string.
+        """
+        ref_names = ref_names or []
+        data_source_rids = {rid_from_instance_or_string(ds) for ds in data_sources or []}
+
+        conjure_run = self._get_latest_api()
+
+        data_sources_to_keep = {
+            ref_name: scout_run_api.CreateRunDataSource(
+                data_source=rds.data_source,
+                series_tags=rds.series_tags,
+                offset=rds.offset,
+            )
+            for ref_name, rds in conjure_run.data_sources.items()
+            if ref_name not in ref_names
+            and (rds.data_source.dataset or rds.data_source.connection or rds.data_source.video) not in data_source_rids
+        }
+
+        updated_run = self._clients.run.update_run(
+            self._clients.auth_header,
+            scout_run_api.UpdateRunRequest(
+                assets=[],
+                data_sources=data_sources_to_keep,
+            ),
+            self.rid,
+        )
+        self._refresh_from_api(updated_run)
 
     def add_dataset(
         self,
@@ -124,168 +198,6 @@ class Run(HasRid, RefreshableMixin[scout_run_api.Run]):
         }
         self._clients.run.add_data_sources_to_run(self._clients.auth_header, data_sources, self.rid)
 
-    def _iter_list_datasets(self) -> Iterable[tuple[str, Dataset]]:
-        dataset_rids_by_ref_name = self._list_datasource_rids("dataset")
-        datasets_by_rids = {
-            ds.rid: Dataset._from_conjure(self._clients, ds)
-            for ds in _get_datasets(self._clients.auth_header, self._clients.catalog, dataset_rids_by_ref_name.values())
-        }
-        for ref_name, rid in dataset_rids_by_ref_name.items():
-            dataset = datasets_by_rids[rid]
-            yield (ref_name, dataset)
-
-    def list_datasets(self) -> Sequence[tuple[str, Dataset]]:
-        """List the datasets associated with this run.
-        Returns (ref_name, dataset) pairs for each dataset.
-        """
-        return list(self._iter_list_datasets())
-
-    def add_video(self, ref_name: str, video: Video | str) -> None:
-        """Add a video to a run via video object or RID."""
-        request = scout_run_api.CreateRunDataSource(
-            data_source=scout_run_api.DataSource(video=rid_from_instance_or_string(video)),
-            series_tags={},
-            offset=None,
-        )
-        self._clients.run.add_data_sources_to_run(self._clients.auth_header, {ref_name: request}, self.rid)
-
-    def _iter_list_videos(self) -> Iterable[tuple[str, Video]]:
-        video_rids_by_ref_name = self._list_datasource_rids("video")
-        videos_by_rids = {
-            rid: Video._from_conjure(
-                self._clients,
-                _get_video(self._clients, rid),
-            )
-            for rid in video_rids_by_ref_name.values()
-        }
-        for ref_name, rid in video_rids_by_ref_name.items():
-            video = videos_by_rids[rid]
-            yield (ref_name, video)
-
-    def list_videos(self) -> Sequence[tuple[str, Video]]:
-        """List a sequence of refname, Video tuples associated with this Run."""
-        return list(self._iter_list_videos())
-
-    def add_attachments(self, attachments: Iterable[Attachment] | Iterable[str]) -> None:
-        """Add attachments that have already been uploaded to this run.
-
-        `attachments` can be `Attachment` instances, or attachment RIDs.
-        """
-        rids = [rid_from_instance_or_string(a) for a in attachments]
-        request = scout_run_api.UpdateAttachmentsRequest(attachments_to_add=rids, attachments_to_remove=[])
-        self._clients.run.update_run_attachment(self._clients.auth_header, request, self.rid)
-
-    def remove_attachments(self, attachments: Iterable[Attachment] | Iterable[str]) -> None:
-        """Remove attachments from this run.
-        Does not remove the attachments from Nominal.
-
-        `attachments` can be `Attachment` instances, or attachment RIDs.
-        """
-        rids = [rid_from_instance_or_string(a) for a in attachments]
-        request = scout_run_api.UpdateAttachmentsRequest(attachments_to_add=[], attachments_to_remove=rids)
-        self._clients.run.update_run_attachment(self._clients.auth_header, request, self.rid)
-
-    def _iter_list_attachments(self) -> Iterable[Attachment]:
-        run = self._get_latest_api()
-        for a in _iter_get_attachments(self._clients.auth_header, self._clients.attachment, run.attachments):
-            yield Attachment._from_conjure(self._clients, a)
-
-    def list_attachments(self) -> Sequence[Attachment]:
-        """List a sequence of Attachments associated with this Run."""
-        return list(self._iter_list_attachments())
-
-    def _iter_list_assets(self) -> Iterable[Asset]:
-        run = self._get_latest_api()
-        assets = self._clients.assets.get_assets(self._clients.auth_header, run.assets)
-        for a in assets.values():
-            yield Asset._from_conjure(self._clients, a)
-
-    def list_assets(self) -> Sequence[Asset]:
-        """List assets associated with this run."""
-        return list(self._iter_list_assets())
-
-    def archive(self) -> None:
-        """Archive this run.
-        Archived runs are not deleted, but are hidden from the UI.
-
-        NOTE: currently, it is not possible (yet) to unarchive a run once archived.
-        """
-        self._clients.run.archive_run(self._clients.auth_header, self.rid)
-
-    def update(
-        self,
-        *,
-        name: str | None = None,
-        start: datetime | IntegralNanosecondsUTC | None = None,
-        end: datetime | IntegralNanosecondsUTC | None = None,
-        description: str | None = None,
-        properties: Mapping[str, str] | None = None,
-        labels: Sequence[str] | None = None,
-        links: Sequence[str | Link | LinkDict] | None = None,
-    ) -> Self:
-        """Replace run metadata.
-        Updates the current instance, and returns it.
-        Only the metadata passed in will be replaced, the rest will remain untouched.
-
-        Links can be URLs, tuples of (URL, name), or dicts of {url=URL, title=name}.
-
-        Note: This replaces the metadata rather than appending it. To append to labels or properties, merge them before
-        calling this method. E.g.:
-
-            new_labels = ["new-label-a", "new-label-b"]
-            for old_label in run.labels:
-                new_labels.append(old_label)
-            run = run.update(labels=new_labels)
-        """
-        request = scout_run_api.UpdateRunRequest(
-            description=description,
-            labels=None if labels is None else list(labels),
-            properties=None if properties is None else dict(properties),
-            start_time=None if start is None else _SecondsNanos.from_flexible(start).to_scout_run_api(),
-            end_time=None if end is None else _SecondsNanos.from_flexible(end).to_scout_run_api(),
-            title=name,
-            assets=[],
-            links=None if links is None else create_links(links),
-        )
-        updated_run = self._clients.run.update_run(self._clients.auth_header, request, self.rid)
-        return self._refresh_from_api(updated_run)
-
-    def remove_data_sources(
-        self,
-        *,
-        ref_names: Sequence[str] | None = None,
-        data_sources: Sequence[Connection | Dataset | Video | str] | None = None,
-    ) -> None:
-        """Remove data sources from this run.
-
-        The list data_sources can contain Connection, Dataset, Video instances, or rids as string.
-        """
-        ref_names = ref_names or []
-        data_source_rids = {rid_from_instance_or_string(ds) for ds in data_sources or []}
-
-        conjure_run = self._get_latest_api()
-
-        data_sources_to_keep = {
-            ref_name: scout_run_api.CreateRunDataSource(
-                data_source=rds.data_source,
-                series_tags=rds.series_tags,
-                offset=rds.offset,
-            )
-            for ref_name, rds in conjure_run.data_sources.items()
-            if ref_name not in ref_names
-            and (rds.data_source.dataset or rds.data_source.connection or rds.data_source.video) not in data_source_rids
-        }
-
-        updated_run = self._clients.run.update_run(
-            self._clients.auth_header,
-            scout_run_api.UpdateRunRequest(
-                assets=[],
-                data_sources=data_sources_to_keep,
-            ),
-            self.rid,
-        )
-        self._refresh_from_api(updated_run)
-
     def add_connection(
         self,
         ref_name: str,
@@ -315,6 +227,40 @@ class Run(HasRid, RefreshableMixin[scout_run_api.Run]):
         }
         self._clients.run.add_data_sources_to_run(self._clients.auth_header, data_sources, self.rid)
 
+    def add_video(self, ref_name: str, video: Video | str) -> None:
+        """Add a video to a run via video object or RID."""
+        request = scout_run_api.CreateRunDataSource(
+            data_source=scout_run_api.DataSource(video=rid_from_instance_or_string(video)),
+            series_tags={},
+            offset=None,
+        )
+        self._clients.run.add_data_sources_to_run(self._clients.auth_header, {ref_name: request}, self.rid)
+
+    def add_attachments(self, attachments: Iterable[Attachment] | Iterable[str]) -> None:
+        """Add attachments that have already been uploaded to this run.
+
+        `attachments` can be `Attachment` instances, or attachment RIDs.
+        """
+        rids = [rid_from_instance_or_string(a) for a in attachments]
+        request = scout_run_api.UpdateAttachmentsRequest(attachments_to_add=rids, attachments_to_remove=[])
+        self._clients.run.update_run_attachment(self._clients.auth_header, request, self.rid)
+
+    def _iter_list_datasets(self) -> Iterable[tuple[str, Dataset]]:
+        dataset_rids_by_ref_name = self._list_datasource_rids("dataset")
+        datasets_by_rids = {
+            ds.rid: Dataset._from_conjure(self._clients, ds)
+            for ds in _get_datasets(self._clients.auth_header, self._clients.catalog, dataset_rids_by_ref_name.values())
+        }
+        for ref_name, rid in dataset_rids_by_ref_name.items():
+            dataset = datasets_by_rids[rid]
+            yield (ref_name, dataset)
+
+    def list_datasets(self) -> Sequence[tuple[str, Dataset]]:
+        """List the datasets associated with this run.
+        Returns (ref_name, dataset) pairs for each dataset.
+        """
+        return list(self._iter_list_datasets())
+
     def _iter_list_connections(self) -> Iterable[tuple[str, Connection]]:
         conn_rids_by_ref_name = self._list_datasource_rids("connection")
         connections_by_rids = {
@@ -331,6 +277,60 @@ class Run(HasRid, RefreshableMixin[scout_run_api.Run]):
         Returns (ref_name, connection) pairs for each connection
         """
         return list(self._iter_list_connections())
+
+    def _iter_list_videos(self) -> Iterable[tuple[str, Video]]:
+        video_rids_by_ref_name = self._list_datasource_rids("video")
+        videos_by_rids = {
+            rid: Video._from_conjure(
+                self._clients,
+                _get_video(self._clients, rid),
+            )
+            for rid in video_rids_by_ref_name.values()
+        }
+        for ref_name, rid in video_rids_by_ref_name.items():
+            video = videos_by_rids[rid]
+            yield (ref_name, video)
+
+    def list_videos(self) -> Sequence[tuple[str, Video]]:
+        """List a sequence of refname, Video tuples associated with this Run."""
+        return list(self._iter_list_videos())
+
+    def _iter_list_attachments(self) -> Iterable[Attachment]:
+        run = self._get_latest_api()
+        for a in _iter_get_attachments(self._clients.auth_header, self._clients.attachment, run.attachments):
+            yield Attachment._from_conjure(self._clients, a)
+
+    def list_attachments(self) -> Sequence[Attachment]:
+        """List a sequence of Attachments associated with this Run."""
+        return list(self._iter_list_attachments())
+
+    def _iter_list_assets(self) -> Iterable[Asset]:
+        run = self._get_latest_api()
+        assets = self._clients.assets.get_assets(self._clients.auth_header, run.assets)
+        for a in assets.values():
+            yield Asset._from_conjure(self._clients, a)
+
+    def list_assets(self) -> Sequence[Asset]:
+        """List assets associated with this run."""
+        return list(self._iter_list_assets())
+
+    def remove_attachments(self, attachments: Iterable[Attachment] | Iterable[str]) -> None:
+        """Remove attachments from this run.
+        Does not remove the attachments from Nominal.
+
+        `attachments` can be `Attachment` instances, or attachment RIDs.
+        """
+        rids = [rid_from_instance_or_string(a) for a in attachments]
+        request = scout_run_api.UpdateAttachmentsRequest(attachments_to_add=[], attachments_to_remove=rids)
+        self._clients.run.update_run_attachment(self._clients.auth_header, request, self.rid)
+
+    def archive(self) -> None:
+        """Archive this run.
+        Archived runs are not deleted, but are hidden from the UI.
+
+        NOTE: currently, it is not possible (yet) to unarchive a run once archived.
+        """
+        self._clients.run.archive_run(self._clients.auth_header, self.rid)
 
     @classmethod
     def _from_conjure(cls, clients: _Clients, run: scout_run_api.Run) -> Self:
