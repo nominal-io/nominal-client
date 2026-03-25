bec9d40c084895c0a02e1159b2004259a947a81b
Kevin Matthews <kevin.matthews@nominal.io>
2026-03-25 17:30:51 -0400

    reject unknown event types when updating events

--- nominal/core/event.py
+++ nominal/core/event.py
@@ -72,6 +72,9 @@ class Event(HasRid, RefreshableMixin[event.Event]):
                 new_labels.append(old_label)
             event = event.update(labels=new_labels)
         """
+
+        if type == EventType.UNKNOWN:
+            raise ValueError("Events with EventType.UNKNOWN cannot be updated")
         request = event.BatchUpdateEventRequest(
             requests=[
                 event.UpdateEventRequest(
