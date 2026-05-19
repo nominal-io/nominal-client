55a64892e16c2a7ce945e5c54ab1e50914496c72
Alexander Reynolds <alex@theory.shop>
2026-05-18 18:47:16 -0700

    remove from 'thirdparty' altogether

---
+++ nominal/thirdparty/tdms/__init__.py
@@ -0,0 +1,15 @@
+import warnings
+
+from nominal.tdms import upload_tdms, upload_tdms_to_dataset
+
+warnings.warn(
+    "nominal.thirdparty.tdms is deprecated and will be removed in a future version. "
+    "Use nominal.tdms instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+__all__ = [
+    "upload_tdms",
+    "upload_tdms_to_dataset",
+]
