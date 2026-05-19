c8778a4f48a01bcb51345eb25a793b48fd637c54
Alexander Reynolds <alex@theory.shop>
2026-05-18 18:50:34 -0700

    fmt

--- nominal/thirdparty/tdms/__init__.py
+++ nominal/thirdparty/tdms/__init__.py
@@ -3,8 +3,7 @@ import warnings
 from nominal.tdms import upload_tdms, upload_tdms_to_dataset
 
 warnings.warn(
-    "nominal.thirdparty.tdms is deprecated and will be removed in a future version. "
-    "Use nominal.tdms instead.",
+    "nominal.thirdparty.tdms is deprecated and will be removed in a future version. Use nominal.tdms instead.",
     DeprecationWarning,
     stacklevel=2,
 )
