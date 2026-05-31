89cedc234d17872f11884c617ae7338c624b38c6
Alexander Reynolds <alex@theory.shop>
2026-05-30 22:24:57 -0700

    add import guard for tdms

--- nominal/thirdparty/tdms/__init__.py
+++ nominal/thirdparty/tdms/__init__.py
@@ -1,6 +1,11 @@
 import warnings
 
-from nominal.tdms import upload_tdms, upload_tdms_to_dataset
+try:
+    from nominal.tdms import upload_tdms, upload_tdms_to_dataset
+except ModuleNotFoundError as e:
+    if e.name in {"nominal.tdms", "nptdms"}:
+        raise ImportError("TDMS support requires the 'tdms' extra: pip install 'nominal[tdms]'") from e
+    raise
 
 warnings.warn(
     "nominal.thirdparty.tdms is deprecated and will be removed in a future version. Use nominal.tdms instead.",
